5cca38491dad57a83e0391c160ace8a593b2198f
Code Sandbox <hello@codesandbox.io>
2022-05-15 17:12:05 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.26fed274bb9609a05104b3b095b3258b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00aa3aaba8d824df08980dd3c2f1f32a",
+    "url": "https://oushesh.github.io/csb-tp9m6y/index.html"
+  },
+  {
+    "revision": "044ec6a3215f420769db",
+    "url": "https://oushesh.github.io/csb-tp9m6y/static/css/2.af4c76d0.chunk.css"
+  },
+  {
+    "revision": "7092c3534918a948be2e",
+    "url": "https://oushesh.github.io/csb-tp9m6y/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "044ec6a3215f420769db",
+    "url": "https://oushesh.github.io/csb-tp9m6y/static/js/2.a6d185d0.chunk.js"
+  },
+  {
+    "revision": "3a03b8602d6970774523596458a848ac",
+    "url": "https://oushesh.github.io/csb-tp9m6y/static/js/2.a6d185d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7092c3534918a948be2e",
+    "url": "https://oushesh.github.io/csb-tp9m6y/static/js/main.26b0896d.chunk.js"
+  },
+  {
+    "revision": "74a3a629b43fdb1a02ca",
+    "url": "https://oushesh.github.io/csb-tp9m6y/static/js/runtime-main.d5e38793.js"
+  },
+  {
+    "revision": "9f2131c2974ff6dcfefe82f5727c466d",
+    "url": "https://oushesh.github.io/csb-tp9m6y/static/media/background.9f2131c2.svg"
+  },
+  {
+    "revision": "7bd9a61f8f1ba09b4d7f957d243470d7",
+    "url": "https://oushesh.github.io/csb-tp9m6y/static/media/compass.7bd9a61f.svg"
+  },
+  {
+    "revision": "adb137f0fa13eb025c13d13c727fe7fa",
+    "url": "https://oushesh.github.io/csb-tp9m6y/static/media/grab.adb137f0.svg"
+  },
+  {
+    "revision": "3acfddd5c09c90a4f089f8d626aebca1",
+    "url": "https://oushesh.github.io/csb-tp9m6y/static/media/grabbing.3acfddd5.svg"
+  },
+  {
+    "revision": "f37aa8ca72b5548a255c62dab1614437",
+    "url": "https://oushesh.github.io/csb-tp9m6y/static/media/sprites.f37aa8ca.svg"
+  }
+]);
